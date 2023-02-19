dcd06fcb5404fc3c597ee4f581480ce0f5a8e51f
shimopino <shimoke4869@gmail.com>
2023-02-19 22:19:15 +0900

    単純な GET /questions エンドポイントを追加

---
+++ Makefile
@@ -0,0 +1,5 @@
+watch:
+	cargo watch -x check -x 'run --bin rust-web-development'
+
+request:
+	cargo run --bin reqwest
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
+use serde::Serialize;
 use std::{
     io::{Error, ErrorKind},
     str::FromStr,
 };
-
 use warp::Filter;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct QuestionId(String);
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -48,37 +48,62 @@ impl std::fmt::Display for QuestionId {
     }
 }
 
-#[tokio::main]
-async fn main() {
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
     let question = Question::new(
         QuestionId::from_str("1").expect("No id provided"),
         "First Question".to_string(),
         "Content of question".to_string(),
         Some(vec!["faq".to_string()]),
     );
-    println!("question sample {:#?}", question);
 
-    let updated_question = question.update_title("Refined First Question".to_string());
-    println!("updated question sample {:#?}", updated_question);
-
-    println!("Question ID => {}", updated_question.id);
-
-    let hello = warp::get().map(|| format!("Hello, World!"));
-
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    Ok(warp::reply::json(&question))
 }
 
-#[test]
-fn test_question_id() {
-    let question_id = QuestionId::from_str("123").unwrap();
+#[tokio::main]
+async fn main() {
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions);
 
-    // QuestionId のままだと等価比較のトレイトを実装していないのでエラーになる
-    assert_eq!(question_id.0, "123".to_string());
+    let routes = get_items;
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
 
-#[test]
-fn test_invalid_question_id() {
-    let empty_question_id = QuestionId::from_str("");
+#[cfg(test)]
+mod tests {
+    use crate::{Question, QuestionId};
+    use std::str::FromStr;
 
-    assert_eq!(empty_question_id.is_err(), true);
+    #[test]
+    fn test_question_id() {
+        let question_id = QuestionId::from_str("123").unwrap();
+
+        // QuestionId のままだと等価比較のトレイトを実装していないのでエラーになる
+        assert_eq!(question_id.0, "123".to_string());
+    }
+
+    #[test]
+    fn test_invalid_question_id() {
+        let empty_question_id = QuestionId::from_str("");
+
+        assert_eq!(empty_question_id.is_err(), true);
+    }
+
+    #[test]
+    fn test_question() {
+        let question = Question::new(
+            QuestionId::from_str("1").expect("No id provided"),
+            "First Question".to_string(),
+            "Content of question".to_string(),
+            Some(vec!["faq".to_string()]),
+        );
+        println!("question sample {:#?}", question);
+
+        let updated_question = question.update_title("Refined First Question".to_string());
+        println!("updated question sample {:#?}", updated_question);
+
+        println!("Question ID => {}", updated_question.id);
+    }
 }
