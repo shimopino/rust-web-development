f3658386cba7f8030949ef148375d097871d7350
shimopino <shimoke4869@gmail.com>
2023-02-21 01:57:09 +0900

    UUIDによるリクエストIDの付与を追加

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ async fn main() {
     log::warn!("This is a warn!");
 
     let log = warp::log::custom(|info| {
-        eprintln!(
+        log::info!(
             "{} {} {} {:?} from {} with {:?}",
             info.method(),
             info.path(),
@@ -31,6 +31,8 @@ async fn main() {
     let store = store::Store::new();
     let store_filter = warp::any().map(move || store.clone());
 
+    let id_filter = warp::any().map(|| uuid::Uuid::new_v4().to_string());
+
     let cors = warp::cors()
         .allow_any_origin()
         .allow_header("content-type")
@@ -41,6 +43,7 @@ async fn main() {
         .and(warp::path::end())
         .and(warp::query())
         .and(store_filter.clone())
+        .and(id_filter)
         .and_then(routes::question::get_questions);
 
     let add_question = warp::post()
